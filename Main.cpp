6d80ae52414735d44fcfe342312006d727a32483
Arya <arya@live.ca>
2018-10-12 23:38:32 -0700

    Add basic command-line argument parsing.

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
 
-int main()
+int main(int argc, char *argv[])
 {
-  std::cout << "Hello, world!\n";
+  if (argc < 2)
+  {
+    std::cerr << "error: not enough arguments\n";
+    std::cerr << "usage: sya [expression]\n";
+    std::exit(-1);
+  }
+
+  const std::string expression = argv[1];
+  std::cout << "expression: " << expression << "\n";
 }
